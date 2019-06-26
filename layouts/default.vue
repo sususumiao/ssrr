022259ac688546c5b7ee8871602791cab6d401ce
sumiao <15976593304@163.com>
2019-06-26 18:01:20 +0800

    01-实现头部跟脚部

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- 头部组件 -->
+    <Header/>
+    <!-- 内容占位组件 -->
+    <nuxt />
+    <!-- 脚部组件 -->
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// 导入头部组件
+import Header from "@/components/header"
+import Footer from "@/components/footer"
+export default {
+  // 注册组件
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+<style lang="less">
+//  全局样式
+*{
+  margin: 0;
+  padding: 0;
+}
+ul,li,ol{
+  list-style: none;
+}
+a{
+  color: #666;
+  text-decoration: none;
+}
+</style>    
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+    首页
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="less" scoped>
+.main{
+  height: 600px;
+}
+</style>
+
+
+
