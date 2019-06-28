98422e4011eff7567e9105c0d62e60bdbb4b9ae1
sumiao <15976593304@163.com>
2019-06-28 18:19:49 +0800

    05-实现登录页面（静态）

---
+++ components/user/LoginForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <el-form
+      :model="formData"
+      :rules="rules"
+      ref="ruleForm"
+      class="demo-ruleForm"
+    >
+      <el-form-item  prop="username">
+        <el-input v-model="formData.username" placeholder="用户名/手机号"></el-input>
+      </el-form-item>
+      <el-form-item  prop="password">
+        <el-input v-model="formData.password" show-password placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+          <el-button type="primary" class="button-primary">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+          username:"",
+          password:""
+      },
+      rules:{
+           username: [
+            { required: true, message: '请输入用户名或手机号', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' }
+          ]
+      }
+    };
+  }
+};
+</script>
+<style lang="less" scoped>
+.demo-ruleForm{
+    padding: 25px;
+    .button-primary{
+        width: 100%;
+    }
+}
+</style>
+
+
--- pages/user/login.vue
+++ pages/user/login.vue
@@ -19,7 +19,7 @@
                 </el-row>
 
                 <!-- 登录功能组件 -->
-                <!-- <LoginForm v-if="currentTab == 0"/> -->
+                <LoginForm v-if="currentTab == 0"/>
 
                 <!-- 注册功能组件 -->
                 <!-- <RegisterForm v-if="currentTab == 1"/> -->
@@ -29,6 +29,7 @@
 </template>
 
 <script>
+import LoginForm from "@/components/user/LoginForm"
 export default {
     data(){
         return {
@@ -39,6 +40,9 @@ export default {
         handleChangeTab(index){
             this.currentTab = index;
         },
+    },
+    components:{
+      LoginForm
     }
 }
 </script>
